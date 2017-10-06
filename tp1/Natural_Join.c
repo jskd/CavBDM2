77549e36cf0310873daa3a28dc55dc6edf8faa9c
jskd <jerome.skoda@hotmail.fr>
2017-10-06 22:55:05 +0200

    add buffermethode

--- tp1/Natural_Join.c
+++ tp1/Natural_Join.c
@@ -8,7 +8,7 @@
 * Nom : Lefranc
 * Prenom : Joaquim
 * email : lefrancjoaquim@gmail.com
-* 
+*
 * Nom : Skoda
 * Prenom : Jérôme
 * email : contact@jeromeskoda.fr
@@ -21,48 +21,79 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * @brief Store the first letter of each line intro buffer
+ * @param[in]  reading file
+ * @param[out] output buffer adress
+ * @param[in]  output buffer max size
+ * @return     size of output buffer
+ */
+size_t storeFileBuffer(FILE* fp, char* buffer, size_t buffer_s) {
+  char* line = NULL;
+  size_t len = 0;
+  size_t wcount= 0;
+
+  while((getline(&line, &len, fp)) != -1) {
+    buffer[wcount]= line[0];
+    wcount++;
+
+    if(wcount >= buffer_s)
+      break;
+  }
+  return wcount;
+}
+
 int main(int argc, char** argv){
 
-	FILE* r_file;
-	FILE* s_file;
-	FILE* rs_file;
+  FILE* r_file;
+  FILE* s_file;
+  FILE* rs_file;
 
-	r_file = fopen("R.txt", "r");
-	if (r_file == NULL)
-		exit(1);
+  r_file = fopen("R.txt", "r");
+  if (r_file == NULL)
+    exit(1);
 
 
-	s_file = fopen("S.txt", "r");
-	if (s_file == NULL)
-		exit(1);
+  s_file = fopen("S.txt", "r");
+  if (s_file == NULL)
+    exit(1);
 
-	rs_file = fopen("RS.txt", "w");
-	if (rs_file == NULL)
-		exit(1);
+  rs_file = fopen("RS.txt", "w");
+  if (rs_file == NULL)
+    exit(1);
 
-	char* r_line = NULL;
-	char* s_line = NULL;
-	size_t r_len = 0;
-	size_t s_len = 0;
 
-	// Parcours de R
-	while (getline(&r_line, &r_len, r_file) != -1) {
+  char* buf = (char*) malloc( 10 * sizeof(char) );
 
-		// Parcours de S
-		while (getline(&s_line, &s_len, s_file) != -1) {
+  size_t k= storeFileBuffer(r_file, buf, 10);
 
-			// Si attribut identique
-			if(strcmp(r_line, s_line) == 0){
-				fprintf(rs_file, "%s", r_line);
-			}
-		}
+  for(int i=0 ; i < 10; i++)
+    printf("%c", buf[i]); 
 
-		rewind(s_file); // Replace le curseur du fichier à 0
-	}
 
-	fclose(r_file);
-	fclose(s_file);
-	fclose(rs_file);
+  char* r_line = NULL;
+  char* s_line = NULL;
+  size_t r_len = 0;
+  size_t s_len = 0;
 
-	exit(0);
+  // Parcours de R
+  while (getline(&r_line, &r_len, r_file) != -1) {
+
+    // Parcours de S
+    while (getline(&s_line, &s_len, s_file) != -1) {
+
+      // Si attribut identique
+      if(strcmp(r_line, s_line) == 0){
+        fprintf(rs_file, "%s", r_line);
+      }
+    }
+
+    rewind(s_file); // Replace le curseur du fichier à 0
+  }
+
+  fclose(r_file);
+  fclose(s_file);
+  fclose(rs_file);
+
+  exit(0);
 }

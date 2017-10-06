b2000bad345e53438673f19efd9502c49457509f
CactusTribe <lefrancjoaquim@gmail.com>
2017-10-06 20:13:07 +0200

    Ajout du makefile

--- tp1/Natural_Join.c
+++ tp1/Natural_Join.c
@@ -45,15 +45,19 @@ int main(int argc, char** argv){
 	size_t r_len = 0;
 	size_t s_len = 0;
 
+	// Parcours de R
 	while (getline(&r_line, &r_len, r_file) != -1) {
 
+		// Parcours de S
 		while (getline(&s_line, &s_len, s_file) != -1) {
+
+			// Si attribut identique
 			if(strcmp(r_line, s_line) == 0){
 				fprintf(rs_file, "%s", r_line);
 			}
 		}
 
-		rewind(s_file);
+		rewind(s_file); // Replace le curseur du fichier à 0
 	}
 
 	fclose(r_file);

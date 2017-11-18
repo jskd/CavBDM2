3b9c01e7d0478532fa69f6362eb58f92a19b2c7b
jskd <git@jeromeskoda.fr>
2017-11-18 15:25:38 +0100

    Update demo-tp1

--- tp6/src/demo/tp1-natural-join.c
+++ tp6/src/demo/tp1-natural-join.c
@@ -20,20 +20,24 @@
 
 static const size_t _buf_size= 10;
 static const size_t _buf_data_lenght= 1;
+static const char* _file_r= "res/demo/tp1/R.txt";
+static const char* _file_s= "res/demo/tp1/S.txt";
+static const char* _file_rs= "res/demo/tp1/RS.txt";
+
 
 int main(int argc, char** argv){
 
   // buffer R.txt
-  struct buffer* buf_r= buffer_read_file("res/R.txt", _buf_size, _buf_data_lenght, BUFFER_CHARACTERS);
+  struct buffer* buf_r= buffer_read_file(_file_r, _buf_size, _buf_data_lenght, BUFFER_CHARACTERS);
   if(buf_r == NULL) {
-    printf("Erreur lors de la lecture de R.txt.\n");
+    printf("Erreur lors de la lecture de %s.\n", _file_r);
     return -1;
   }
 
   // buffer S.txt
-  struct buffer* buf_s= buffer_read_file("res/S.txt", _buf_size, _buf_data_lenght, BUFFER_CHARACTERS);
+  struct buffer* buf_s= buffer_read_file(_file_s, _buf_size, _buf_data_lenght, BUFFER_CHARACTERS);
   if(buf_s == NULL) {
-    printf("Erreur lors de la lecture de S.txt.\n");
+    printf("Erreur lors de la lecture de %s.\n", _file_s);
     return -1;
   }
 
@@ -44,10 +48,10 @@ int main(int argc, char** argv){
   natural_join(buf_r, buf_s, buf_rs);
 
   // Ecriture du resultat dans RS.txt
-  if(buffer_write_file("res/RS.txt", buf_rs))
-    printf("Erreur lors de l'ecriture de RS.txt.\n");
+  if(buffer_write_file(_file_rs, buf_rs))
+    printf("Erreur lors de la lecture de %s.\n", _file_rs);
 
-  printf("Terminé, fichier dans res/RS.txt\n");
+  printf("Terminé, fichier dans %s\n", _file_rs);
 
   // Remove buf
   buffer_destroy(buf_r);

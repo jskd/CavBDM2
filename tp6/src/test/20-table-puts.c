9d3fbc80dd769d884e62e1c406a315ab559e6b70
jskd <git@jeromeskoda.fr>
2017-11-18 16:54:59 +0100

    Clean test 20

--- tp6/src/test/20-table-puts.c
+++ tp6/src/test/20-table-puts.c
@@ -1,12 +1,24 @@
 #include "../bdd/bufferExtended.h"
 #include "../bdd/table.h"
 
+static const size_t _buf_size= 10;
+static const size_t _data_lenght= sizeof(short);
+static const char* _file_r= "res/test/20/R.txt";
+static const char* _file_table_r= "res/test/20/tableR";
+
 int main(int argc, char** argv){
 
-  struct buffer* buf= buffer_read_file("res/test/20-buffer-pair-impaire.txt",
-    10, sizeof(short), BUFFER_DECIMALS);
+  struct buffer* buf= buffer_read_file(_file_r, _buf_size, _data_lenght, BUFFER_DECIMALS);
+  if(buf == NULL) {
+    printf("Erreur lors de la lecture de %s.\n", _file_r);
+    return -1;
+  }
 
-  struct table* tab= table_create(2, "res/test/20-bucket");
+  struct table* tab= table_create(2, _file_table_r);
+  if(tab == NULL) {
+    printf("Erreur lors de la creation de %s.\n", _file_table_r);
+    return -1;
+  }
 
   table_putBuffer(tab, buf);
 

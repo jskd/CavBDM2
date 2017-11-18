98fbdbc4226c85960da7a45fdffaae794a552828
jskd <git@jeromeskoda.fr>
2017-11-18 16:39:59 +0100

    update demo-tp-2

--- tp6/src/demo/tp2-merge-join-without-duplicate.c
+++ tp6/src/demo/tp2-merge-join-without-duplicate.c
@@ -18,44 +18,44 @@
 
 #include "../bdd/mergeJoinWithoutDuplicate.h"
 
-static const size_t buf_size= 10;
+static const size_t _buf_size= 10;
+static const size_t _buf_data_lenght= 1;
+static const char* _file_r= "res/demo/tp2/R.txt";
+static const char* _file_s= "res/demo/tp2/S.txt";
+static const char* _file_rs= "res/demo/tp2/RS.txt";
 
 int main(int argc, char** argv){
 
   // buffer R.txt
-  struct buf* buf_r= storeFileBuffer("res/R.txt", buf_size);
+  struct buffer* buf_r= buffer_read_file(_file_r, _buf_size, _buf_data_lenght, BUFFER_CHARACTERS);
   if(buf_r == NULL) {
-    printf("Erreur lors de la lecture de R.txt.\n");
+    printf("Erreur lors de la lecture de %s.\n", _file_r);
     return -1;
   }
 
   // buffer S.txt
-  struct buf* buf_s= storeFileBuffer("res/S.txt", buf_size);
+  struct buffer* buf_s= buffer_read_file(_file_s, _buf_size, _buf_data_lenght, BUFFER_CHARACTERS);
   if(buf_s == NULL) {
-    printf("Erreur lors de la lecture de S.txt.\n");
+    printf("Erreur lors de la lecture de %s.\n", _file_s);
     return -1;
   }
 
   // buffer de sortie
-  struct buf* buf_rs= buf_create( buf_size);
-
-  // Trie
-  buf_quicksort(buf_r);
-  buf_quicksort(buf_s);
+  struct buffer* buf_rs= buffer_create( _buf_size, _buf_data_lenght, BUFFER_CHARACTERS);
 
   // Jointure
   merge_join_without_duplicate(buf_r, buf_s, buf_rs);
 
   // Ecriture du resultat dans RS.txt
-  if(writeBufferInFile("res/RS.txt", buf_rs))
-    printf("Erreur lors de l'ecriture de RS.txt.\n");
+  if(buffer_write_file(_file_rs, buf_rs))
+    printf("Erreur lors de la lecture de %s.\n", _file_rs);
 
-  printf("Terminé, fichier dans res/RS.txt\n");
+  printf("Terminé, fichier dans %s\n", _file_rs);
 
   // Remove buf
-  buf_destroy(buf_r);
-  buf_destroy(buf_s);
-  buf_destroy(buf_rs);
+  buffer_destroy(buf_r);
+  buffer_destroy(buf_s);
+  buffer_destroy(buf_rs);
 
   return 0;
 }

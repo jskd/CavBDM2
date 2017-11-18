299cd538c4bf492874da4289621c9992c52ca87f
jskd <git@jeromeskoda.fr>
2017-11-18 16:44:57 +0100

    add color test script

--- tp6/script/test.sh
+++ tp6/script/test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 EXPECTED_DIR="src/test"
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 echo "Script de test automatisé"
 for filename in bin/test/*; do
   NAME=$(basename $filename)
-  $filename > "$EXPECTED_DIR/$NAME.out"
+  $filename 1> "$EXPECTED_DIR/$NAME.out" 2>&1
   if diff "$EXPECTED_DIR/$NAME.expected" "$EXPECTED_DIR/$NAME.out" > /dev/null;
   then
-    echo "[PASS] $NAME"
+    echo "${green}[PASS] $NAME${reset}"
   else
-    echo "[FAIL] $NAME ($EXPECTED_DIR/$NAME.out != $EXPECTED_DIR/$NAME.expected)"
+    >&2 echo "${red}[FAIL] $NAME${reset}"
   fi
 done

80cf1da9a1f1cd1c577a21e565da6f8258f74185
jskd <git@jeromeskoda.fr>
2017-11-26 14:16:45 +0100

    Add include guard

--- tp7/src/bdd/diskSortMerge.h
+++ tp7/src/bdd/diskSortMerge.h
@@ -1,8 +1,33 @@
+/**
+* TP n°: 7
+*
+* Titre du TP : Disk Merge Join
+*
+* Date : 24 Nov 2017
+*
+* Nom : Lefranc
+* Prenom : Joaquim
+* email : lefrancjoaquim@gmail.com
+*
+* Nom : Skoda
+* Prenom : Jérôme
+* email : contact@jeromeskoda.fr
+*
+* Remarques :
+*/
+
+#ifndef SRC_BDD_DISK_SORT_MERGE_H
+#define SRC_BDD_DISK_SORT_MERGE_H
 
 
+#include "buffer.h"
+#include "diskManagerWriter.h"
+#include "diskManagerReader.h"
 
 
 
 void disk_explode_and_sort_to_disk_manager(struct diskReader* dr, struct buffer* buf, struct diskManagerWriter* dmw);
 
 void disk_manager_merge_step(struct DiskManagerReader in, struct diskManagerWriter* out);
+
+#endif

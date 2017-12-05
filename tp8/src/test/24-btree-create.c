c720ee56afc22ea143e63a02a6d00c34f8367ea3
jskd <git@jeromeskoda.fr>
2017-12-05 19:05:25 +0100

    Add test

---
+++ tp8/src/test/24-btree-create.c
@@ -0,0 +1,52 @@
+/**
+* TP n°: 5
+*
+* Titre du TP : Nested loop join disk
+*
+* Date : 10 Nov 2017
+*
+* Nom : Lefranc
+* Prenom : Joaquim
+* email : lefrancjoaquim@gmail.com
+*
+* Nom : Skoda
+* Prenom : Jérôme
+* email : contact@jeromeskoda.fr
+*
+* Remarques :
+*/
+
+#include "../bdd/diskSortMerge.h"
+#include "../bdd/BTree.h"
+#include <limits.h>
+
+// input disk config
+static const char* _file_r= "res/test/24/R";
+static const char* _path_output="res/test/24/R-btree";
+
+int main(int argc, char** argv){
+
+ struct diskReader* dr= disk_r_create(_file_r);
+  if(dr == NULL) {
+    printf("Erreur lors de la lecture de %s.\n", _file_r);
+    return -1;
+  }
+
+  struct diskWriter* dw= disk_w_create(_path_output, "", ".node");
+  if(dw == NULL) {
+    printf("Erreur lors de la lecture de %s.\n", _path_output);
+    return -1;
+  }
+
+  struct btree_node* root= btreenode_create(dw);
+
+  for(int i=0; i< disk_r_count(dr); i++) {
+    printf("Insertion de %s.\n", disk_r_item_path(dr, i));
+    btreenode_insert( root, disk_r_item_path(dr, i) , dw );
+    printf("B+tree:\n");
+    print_btree(stdout, root);
+    printf("\n");
+  }
+
+  printf("Génération de l'arbre %s terminé.\n", _path_output);
+}
---
+++ tp8/src/test/25-btree-search.c
@@ -0,0 +1,52 @@
+/**
+* TP n°: 5
+*
+* Titre du TP : Nested loop join disk
+*
+* Date : 10 Nov 2017
+*
+* Nom : Lefranc
+* Prenom : Joaquim
+* email : lefrancjoaquim@gmail.com
+*
+* Nom : Skoda
+* Prenom : Jérôme
+* email : contact@jeromeskoda.fr
+*
+* Remarques :
+*/
+
+#include "../bdd/diskSortMerge.h"
+#include "../bdd/BTree.h"
+#include <limits.h>
+
+// input disk config
+static const char* _path_btree="res/test/25/R-btree";
+
+static void _search_demo(struct btree_node* root, const char* key) {
+  char search[PATH_MAX];
+  btreenode_search(root, key, search) ;
+  printf("Search %s: %s\n", key, search);
+}
+
+int main(int argc, char** argv){
+
+ struct diskReader* dr= disk_r_create(_path_btree);
+  if(dr == NULL) {
+    printf("Erreur lors de la lecture de %s.\n", _path_btree);
+    return -1;
+  }
+
+  struct btree_node* tree = btree_load(dr);
+
+  printf("Exemple de recherche dans l'index %s:\n", _path_btree);
+  _search_demo(tree, "AG");
+  _search_demo(tree, "DV");
+  _search_demo(tree, "IF");
+  _search_demo(tree, "LE");
+  _search_demo(tree, "OO");
+  _search_demo(tree, "RE");
+  _search_demo(tree, "SS");
+  _search_demo(tree, "XD");
+  _search_demo(tree, "ZZ");
+}
